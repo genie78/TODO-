7fdf9b773d87b70f34b26fb72a7cb61397ac1eef
genie78 <gusein.guseinov2002@gmail.com>
2022-04-09 20:03:15 -0400

    js done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,27 @@ $(document).ready(function () {
     let listItem = $('input');
     let inputDiv = $('.input-div');
     let errorDiv = $('.error-div');
-    let numberOfValue = 0;
-
+    let todoList = [];
+    let indexOfArr = 0;
 
     errorDiv.hide();
 
     const addToList = () => {
 
-        if (listItem.val() == ''){
+        if (listItem.val().toLowerCase() == ''){
             errorDiv.show(2000);
             setTimeout(()=> errorDiv.hide(2000), 2000);
         }else {
             let newDiv = $('<div>');
-            newDiv.attr('value', listItem.val());
+            newDiv.attr('value', listItem.val().toLowerCase());
             newDiv.addClass('input-group input-div border-0');
             inputDiv.append(newDiv);
 
             newDiv.append(
-                `<input type="text" disabled value="${listItem.val()}" class="form-control border-0" >
+                `<input type="text" disabled value="${listItem.val().toLowerCase()}" class="${listItem.val().toLowerCase()} form-control border-0" >
                     
                 <div class="input-group-append">
-                    <button value="${listItem.val()}" type="button" class="close border border-secondary h-50 d-flex align-items-center rounded-circle remove-button mx-3" >
+                    <button value="${listItem.val().toLowerCase()}" type="button" class="${listItem.val().toLowerCase()} close border border-secondary h-50 d-flex align-items-center rounded-circle remove-button mx-3" >
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>`
@@ -34,20 +34,45 @@ $(document).ready(function () {
 
     const clearInput = () => listItem.val('');
 
-    var removeFromList = function () {
-        var valueOfElement = $(this).val();
+    const removeFromList = function () {
+        let valueOfElement = $(this).val();
         for (let i of $('div')){
             let targetDivsValue = i.getAttribute('value');
             if (targetDivsValue == valueOfElement){
                 i.remove();
+                todoList.splice(todoList.indexOf(targetDivsValue), 1);                
             }
         }
-        console.log(valueOfElement);
+    }
+
+    const sortList = () => {
+        clearInput();
+
+        for (let i of $('div')){
+            let targetDivsValue = i.getAttribute('value');
+            if (targetDivsValue != undefined){
+                if (todoList.indexOf(targetDivsValue) == '-1'){
+                    todoList.push(targetDivsValue);
+                    console.log(targetDivsValue);
+                }
+            }
+        }
+        todoList.sort();
+                
+        for (let i of $('div')){
+            let targetDivsValue = i.getAttribute('value');
+            if (targetDivsValue != undefined){
+                i.setAttribute('value', todoList[indexOfArr]);
+                $(`.${targetDivsValue}`).val(todoList[indexOfArr]);
+                indexOfArr++;
+            }
+        }
     }
     
     $(document).on('click', 'button.remove-button' , removeFromList);
     $('.add-button').on('click', addToList);
     $('.close-button').on('click', clearInput);
+    $('.sort-button').on('click', sortList);
 });
 
 
